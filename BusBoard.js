c40211ffe0d092826bffd0cf028330094c593f10
mmyleung <minamleung@gmail.com>
2023-09-30 12:58:32 +0100

    retrieve stoppointId from postcode

--- BusBoard.js
+++ BusBoard.js
@@ -2,14 +2,37 @@ import fetch from 'node-fetch';
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
-let stopPointId = prompt("Which stop point would you like to search for?");
+let postCode = prompt("Which postcode would you like to search for?");
 
-async function fetchLiveArrivals() {
-    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopPointId}/Arrivals`)
+async function fetchLatLon() {
+    const response = await fetch(`https://api.postcodes.io/postcodes/${postCode}`)
     return await response.json();
 }
 
-const arrivalData = await fetchLiveArrivals()
-    arrivalData.sort((a, b) => a.timeToStation - b.timeToStation);
-    arrivalData.slice(0, 5).map(arrival => 
-    console.log(`Bus ${arrival.lineName} to ${arrival.towards} arriving in ${arrival.timeToStation}`))
+async function fetchStopPoints() {
+    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}&stopTypes=NaptanPublicBusCoachTram&radius=50`)
+    const data = await response.json();
+    return data.stopPoints;
+}
+
+const latLonData = await fetchLatLon();
+const lat = latLonData.result.latitude;
+const lon = latLonData.result.longitude;
+
+// async function fetchLiveArrivals() {
+//     const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopPointId}/Arrivals`)
+//     return await response.json();
+// }
+
+// const arrivalData = await fetchLiveArrivals()
+//     arrivalData.sort((a, b) => a.timeToStation - b.timeToStation);
+//     arrivalData.slice(0, 5).map(arrival => 
+//     console.log(`Bus ${arrival.lineName} to ${arrival.towards} arriving in ${arrival.timeToStation}`))
+
+const stopPoints = await fetchStopPoints();
+const stopPointIds = [];
+stopPoints.map((stop) => {
+    stopPointIds.push(stop.naptanId)
+})
+
+console.log(stopPointIds);
