e2635e5a26ab7ca87f62db90f0522f969a45dce2
mmyleung <minamleung@gmail.com>
2023-09-30 14:33:09 +0100

    error handling for no bus stops or buses

--- BusBoard.js
+++ BusBoard.js
@@ -15,6 +15,11 @@ async function fetchStopPoints(lat, lon) {
     return data.stopPoints;
 }
 
+async function fetchLiveArrivals(id) {
+    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${id}/Arrivals`)
+    return await response.json();
+}
+
 const latLonData = await fetchLatLon(postCode);
 if(latLonData.status === 404) {
     console.log("Please enter a valid postcode")
@@ -23,24 +28,30 @@ if(latLonData.status === 404) {
     const lon = latLonData.result.longitude;
     const stopPoints = await fetchStopPoints(lat, lon);
     const stopPointIds = [];
-    stopPoints.map((stop) => {
-        stopPointIds.push(stop.naptanId)
-    })
-
-    stopPointIds.map(async (id) => {
-        const arrivalData = await fetchLiveArrivals(id);
-        console.log(`${arrivalData[0].stationName} (${arrivalData[0].platformName})`);
-        arrivalData.sort((a, b) => a.timeToStation - b.timeToStation);
-        arrivalData.slice(0, 5).map(arrival => 
-        console.log(`Bus ${arrival.lineName} to ${arrival.towards} arriving in ${arrival.timeToStation}`))
-    })
+    if(!stopPoints) {
+        console.log("Sorry, there are no bus stops nearby.")
+    } else {
+        stopPoints.map((stop) => {
+            stopPointIds.push(stop.naptanId)
+        })
+    
+        stopPointIds.map(async (id) => {
+            const arrivalData = await fetchLiveArrivals(id);
+            console.log(`${arrivalData[0].stationName} (${arrivalData[0].platformName})`);
+            if (!arrivalData) {
+                console.log("Sorry there are no buses arriving")
+            } else {
+                arrivalData.sort((a, b) => a.timeToStation - b.timeToStation);
+                arrivalData.slice(0, 5).map(arrival => 
+                console.log(`Bus ${arrival.lineName} to ${arrival.towards} arriving in ${arrival.timeToStation}`))
+            }      
+        })
+    }
+    
 }
 
 
-async function fetchLiveArrivals(id) {
-    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${id}/Arrivals`)
-    return await response.json();
-}
+
 
 
 
